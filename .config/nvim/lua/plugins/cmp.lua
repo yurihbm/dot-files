e57ea83665e11787270dd5ea64f523afc55d98c1
Yuri Maciel <35900428+yurihbm@users.noreply.github.com>
2025-01-07 15:18:40 -0300

    chore: general plugins config improvements

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,67 +1,76 @@
 vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#AFAFAF" })
 
 return {
-	"hrsh7th/nvim-cmp",
-	opts = function()
-		local lspkind = require("lspkind")
-		local cmp = require("cmp")
+	{
+		"folke/lazydev.nvim",
+		ft = "lua", -- only load on lua files
+		opts = {},
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		opts = function()
+			local lspkind = require("lspkind")
+			local cmp = require("cmp")
 
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					require("luasnip").lsp_expand(args.body)
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					-- ['<C-b>'] = cmp.mapping.scroll_docs(-4).
+					-- ['<C-f>'] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({
+						select = false, -- `false` to prevent auto selecting the first item.
+						behavior = cmp.ConfirmBehavior.Replace,
+					}),
+				}),
+				sources = cmp.config.sources({
+					{ name = "copilot" },
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "buffer" },
+				}),
+				---@diagnostic disable-next-line: missing-fields
+				formatting = {
+					format = lspkind.cmp_format({
+						mode = "symbol",
+						with_text = true,
+						ellipsis_char = "...",
+						show_labelDetails = true,
+						maxwidth = function()
+							return math.floor(0.45 * vim.o.columns)
+						end,
+					}),
+				},
+			})
+		end,
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			{
+
+				"onsails/lspkind.nvim",
+				config = function()
+					local lspkind = require("lspkind")
+					lspkind.init({
+						symbol_map = {
+							Copilot = "",
+						},
+					})
 				end,
 			},
-			mapping = cmp.mapping.preset.insert({
-				-- ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-				-- ['<C-f>'] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(),
-				["<C-e>"] = cmp.mapping.abort(),
-				["<CR>"] = cmp.mapping.confirm({ select = false }), -- `true` to auto select first item. `false` to only confirm explicitly selected items.
-			}),
-			sources = cmp.config.sources({
-				{ name = "copilot" },
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
-				{
-					{ name = "buffer" },
+			{
+				"L3MON4D3/LuaSnip",
+				dependencies = {
+					"saadparwaiz1/cmp_luasnip",
 				},
-			}),
-			formatting = {
-				format = lspkind.cmp_format({
-					mode = "symbol",
-					with_text = true,
-					ellipsis_char = "...",
-					show_labelDetails = true,
-					maxwidth = function()
-						return math.floor(0.45 * vim.o.columns)
-					end,
-				}),
-			},
-		})
-	end,
-	dependencies = {
-		"neovim/nvim-lspconfig",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-cmdline",
-		{
-
-			"onsails/lspkind.nvim",
-			config = function()
-				local lspkind = require("lspkind")
-				lspkind.init({
-					symbol_map = {
-						Copilot = "",
-					},
-				})
-			end,
-		},
-		{
-			"L3MON4D3/LuaSnip",
-			dependencies = {
-				"saadparwaiz1/cmp_luasnip",
 			},
 		},
 	},
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,6 +3,7 @@ return {
 	build = ":TSUpdate",
 	config = function()
 		local configs = require("nvim-treesitter.configs")
+		---@diagnostic disable-next-line: missing-fields
 		configs.setup({
 			ensure_installed = {
 				"c",
