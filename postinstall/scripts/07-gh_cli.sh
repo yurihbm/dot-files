6c875bdc6ac32d5f12c0396e75b65d91f89a0e67
Yuri Henrique Bernardes Maciel <35900428+yurihbm@users.noreply.github.com>
2025-04-19 17:44:31 -0300

    fix: set gh token variable as temp

--- postinstall/scripts/07-gh_cli.sh
+++ postinstall/scripts/07-gh_cli.sh
@@ -4,17 +4,19 @@ set -e
 
 source ./scripts/helpers/status.sh
 
+trap 'unset GH_TEMP_TOKEN' EXIT
+
 echo -e "\nInstalling github-cli..."
 
-if [[ -z "$GH_TOKEN" ]]; then
-echo -e "\nError: GH_TOKEN not defined. Please set it in your environment."
+if [[ -z "$GH_TEMP_TOKEN" ]]; then
+echo -e "\nError: GH_TEMP_TOKEN not defined. Please set it in your environment."
   exit 1
 fi
 
 sudo dnf config-manager addrepo --from-repofile=https://cli.github.com/packages/rpm/gh-cli.repo
 sudo dnf install gh --repo gh-cli -y
 
-echo "$GH_TOKEN" | gh auth login -p ssh --skip-ssh-key --with-token
+echo "$GH_TEMP_TOKEN" | gh auth login -p ssh --skip-ssh-key --with-token
 gh ssh-key add ~/.ssh/id_github_ed25519;
 
 gh extension install github/gh-copilot
