56fd8c18f18c3e1a0e92ee54f8381023830afb43
Yuri Henrique Bernardes Maciel <35900428+yurihbm@users.noreply.github.com>
2025-04-19 19:50:27 -0300

    refactor: improve formatting and add quiet flags

--- postinstall/scripts/05-go.sh
+++ postinstall/scripts/05-go.sh
@@ -7,8 +7,8 @@ source ./scripts/helpers/status.sh
 echo -e "\nInstalling Go..."
 
 if command -v go >/dev/null 2>&1; then
-    echo -e "\nGo is already installed. Skipping..."
-    return 0
+   echo -e "\nGo is already installed. Skipping..."
+   return 0
 fi
 
 wget "https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz"
@@ -19,4 +19,3 @@ rm -f "go${GO_VERSION}.linux-amd64.tar.gz"
 echo -e "\nGo ${GO_VERSION} installed."
 
 clear
-
