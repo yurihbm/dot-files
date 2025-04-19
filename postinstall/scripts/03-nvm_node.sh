514c5d2274f4f0f28977b9015340cb41192d3a82
Yuri Henrique Bernardes Maciel <35900428+yurihbm@users.noreply.github.com>
2025-04-19 18:39:52 -0300

    fix: create dir before install

--- postinstall/scripts/03-nvm_node.sh
+++ postinstall/scripts/03-nvm_node.sh
@@ -4,6 +4,8 @@ set -e
 
 source ./scripts/helpers/status.sh
 
+mkdir -p $NVM_DIR
+
 echo -e "\nInstalling NVM and latest LTS Node..."
 curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/v${NVM_VERSION}/install.sh" | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
