a162a402ed1824049d2250b3c5dfdedfc3ae0970
Yuri Henrique Bernardes Maciel <35900428+yurihbm@users.noreply.github.com>
2025-04-19 19:04:47 -0300

    fix: remove invalid export

--- postinstall/scripts/03-nvm_node.sh
+++ postinstall/scripts/03-nvm_node.sh
@@ -14,8 +14,9 @@ fi
 mkdir -p $NVM_DIR
 
 curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/v${NVM_VERSION}/install.sh" | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install --lts
+
 nvm install --lts
 corepack enable
 
