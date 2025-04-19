c3ea143fbff82d478d7fe459137c524501290ca6
Yuri Henrique Bernardes Maciel <35900428+yurihbm@users.noreply.github.com>
2025-04-19 18:40:00 -0300

    feat: reload udev rules

--- postinstall/scripts/15-solaar.sh
+++ postinstall/scripts/15-solaar.sh
@@ -11,6 +11,8 @@ sudo dnf install solaar libX11-devel -y
 wget https://raw.githubusercontent.com/pwr-Solaar/Solaar/master/rules.d-uinput/42-logitech-unify-permissions.rules
 sudo mv 42-logitech-unify-permissions.rules /etc/udev/rules.d/
 
+sudo udevadm control --reload-rules && sudo udevadm trigger
+
 echo -e "\nSolaar installed."
 
 clear
