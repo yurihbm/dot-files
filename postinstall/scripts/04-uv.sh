d5d3249c56547119319d1910ea414691f62493c8
Yuri Henrique B. Maciel <35900428+yurihbm@users.noreply.github.com>
2025-05-02 11:29:05 -0300

    refactor: improve zsh config and use uv instead of pyenv

---
+++ postinstall/scripts/04-uv.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+source ./scripts/helpers/status.sh
+
+echo -e "\nInstalling uv..."
+
+if command -v uv >/dev/null 2>&1; then
+   echo -e "\nuv is already installed. Skipping..."
+   return 0
+fi
+
+curl -LsSf https://astral.sh/uv/install.sh | env UV_INSTALL_DIR="$UV_DIR" sh
+
+echo -e "\nuv installed."
+
+clear
