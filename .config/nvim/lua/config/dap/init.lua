1cc802dc613ed95633152b9c302f993c5f8728c4
Yuri Henrique B. Maciel <35900428+yurihbm@users.noreply.github.com>
2025-05-10 12:20:37 -0300

    refactor(nvim): improve config structure and lazy loading

---
+++ .config/nvim/lua/config/dap/init.lua
@@ -0,0 +1,20 @@
+local dap, dapui = require("dap"), require("dapui")
+
+dapui.setup()
+
+dap.listeners.before.attach.dapui_config = function()
+	dapui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+	dapui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+	dapui.close()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+	dapui.close()
+end
+
+require("config.dap.python")
+require("config.dap.go")
+require("config.dap.js_ts")
