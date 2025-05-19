525b8dc852849412ae669d055244ac76fe28180b
Yuri Henrique B. Maciel <35900428+yurihbm@users.noreply.github.com>
2025-05-19 02:12:58 -0300

    fix(postinstall): remove invalid quotes

--- postinstall/scripts/17-config.sh
+++ postinstall/scripts/17-config.sh
@@ -6,7 +6,7 @@ source ./scripts/helpers/status.sh
 
 echo -e "\nImporting configs..."
 
-cp "../.config/solaar/*" "$HOME/.config/solaar/"
+cp ../.config/solaar/* "$HOME/.config/solaar/"
 
 cp -r "../.config/tmux" "$HOME/.config/tmux"
 
