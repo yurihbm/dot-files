9e80c7206ac87a3aace5cad5eb1b11a2d7846728
Yuri Henrique B. Maciel <35900428+yurihbm@users.noreply.github.com>
2025-11-18 15:35:59 -0300

    fix: remove unused array

--- postinstall/scripts/02-flatpak.sh
+++ postinstall/scripts/02-flatpak.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-APPS=(
-  "com.belmoussaoui.Authenticator"
-  "com.discordapp.Discord"
-  "com.github.tchx84.Flatseal"
-  "com.google.Chrome"
-  "com.mattjakeman.ExtensionManager"
-  "com.valvesoftware.Steam"
-  "hu.irl.cameractrls"
-  "rest.insomnia.Insomnia"
-  "io.github.pwr_solaar.solaar"
-)
-
 flatpak install -y --noninteractive \
   com.belmoussaoui.Authenticator \
   com.discordapp.Discord \
