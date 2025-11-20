a7cce1767e53205c393082a82447c9457cc89876
Yuri Henrique B. Maciel <35900428+yurihbm@users.noreply.github.com>
2025-11-20 11:03:10 -0300

    fix: use https for git clone

--- postinstall/scripts/04-theme.sh
+++ postinstall/scripts/04-theme.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 echo -e "\dDownloading WhiteSur Icon Theme..."
 
 USER_ICONS_FOLDER="$HOME/.local/icons"
@@ -9,7 +8,7 @@ mkdir -p "$USER_ICONS_FOLDER"
 
 WHITESUR_ICON_THEME_FOLDER="$USER_ICONS_FOLDER/whitesur-icon-theme"
 
-git clone git@github.com:vinceliuice/WhiteSur-icon-theme.git "$WHITESUR_ICON_THEME_FOLDER"
+git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git "$WHITESUR_ICON_THEME_FOLDER"
 
 echo -e "\nCopying custom icons to WhiteSur Icon Theme..."
 
