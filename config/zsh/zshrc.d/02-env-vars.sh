f90be4de2e5473de1884ce7a78a33d4d572d5353
Yuri Henrique B. Maciel <35900428+yurihbm@users.noreply.github.com>
2025-12-01 13:36:46 -0300

    fix: add proper source order for zsh config files

--- config/zsh/zshrc.d/02-env-vars.sh
+++ config/zsh/zshrc.d/02-env-vars.sh
@@ -14,3 +14,12 @@ export DOCKER_CONFIG="$HOME/.config/docker"
 export NPM_CONFIG_CACHE="$HOME/.cache/npm"
 export NPM_CONFIG_USERCONFIG="$HOME/.config/npm/.npmrc"
 export NPM_CONFIG_INIT_MODULE="$HOME/.config/npm/.npm-init.js"
+
+# ========== Yarn Config ==========
+
+export YARN_GLOBAL_FOLDER="$HOME/.local/share/yarn"
+export YARN_ENABLE_TELEMETRY=false
+
+# ========== GPG Config ==========
+
+export GPG_TTY=$(tty)
