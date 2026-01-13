c0f94e72156e195382b599c437828d2899a4f776
Yuri Henrique B. Maciel <35900428+yurihbm@users.noreply.github.com>
2026-01-12 22:09:27 -0300

    feat(nvim): add lsp keymaps

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -4,24 +4,69 @@
 
 -- This file provides a collection of keymaps for various modes.
 
+local del = vim.keymap.del
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 -- =========================
 -- LSP Keymaps
 -- =========================
-keymap("n", "gd", vim.lsp.buf.definition, vim.tbl_extend("force", opts, { desc = "[G]o to [D]efinition" }))
+keymap("n", "gn", vim.lsp.buf.rename, vim.tbl_extend("force", opts, { desc = "Rename" }))
+keymap("n", "ga", vim.lsp.buf.code_action, vim.tbl_extend("force", opts, { desc = "Show Code Actions" }))
+keymap("n", "gO", vim.lsp.buf.definition, vim.tbl_extend("force", opts, { desc = "Show Symbol Usage" }))
+keymap("n", "gd", vim.lsp.buf.definition, vim.tbl_extend("force", opts, { desc = "Go to Definition" }))
 keymap("n", "gh", function()
 	vim.cmd("split")
 	vim.lsp.buf.definition()
-end, vim.tbl_extend("force", opts, { desc = "[G]o to Definition ([H]orizontal Split)" }))
+end, vim.tbl_extend("force", opts, { desc = "Go to Definition (split)" }))
 keymap("n", "gv", function()
 	vim.cmd("vsplit")
 	vim.lsp.buf.definition()
-end, vim.tbl_extend("force", opts, { desc = "[G]o to Definition ([V]ertical Split)" }))
+end, vim.tbl_extend("force", opts, { desc = "Go to Definition (vsplit)" }))
+
+-- =========================
+-- Intuitive Split Resizing Keymaps
+-- =========================
+keymap("n", "<C-Up>", function()
+	if vim.fn.winnr("j") == vim.fn.winnr() then
+		vim.cmd("resize +2")
+	else
+		vim.cmd("resize -2")
+	end
+end, vim.tbl_extend("force", opts, { desc = "Resize Split Up" }))
+keymap("n", "<C-Down>", function()
+	if vim.fn.winnr("j") == vim.fn.winnr() then
+		vim.cmd("resize -2")
+	else
+		vim.cmd("resize +2")
+	end
+end, vim.tbl_extend("force", opts, { desc = "Resize Split Down" }))
+keymap("n", "<C-Left>", function()
+	if vim.fn.winnr("l") == vim.fn.winnr() then
+		vim.cmd("vertical resize +2")
+	else
+		vim.cmd("vertical resize -2")
+	end
+end, vim.tbl_extend("force", opts, { desc = "Resize Split Left" }))
+keymap("n", "<C-Right>", function()
+	if vim.fn.winnr("l") == vim.fn.winnr() then
+		vim.cmd("vertical resize -2")
+	else
+		vim.cmd("vertical resize +2")
+	end
+end, vim.tbl_extend("force", opts, { desc = "Resize Split Right" }))
 
 -- =========================
 -- Clipboard Keymaps
 -- =========================
-keymap("v", "<leader>y", '"+y', vim.tbl_extend("force", opts, { desc = "Cop[y] to System Clipboard" }))
-keymap("n", "<leader>p", '"+p', vim.tbl_extend("force", opts, { desc = "[P]aste from System Clipboard" }))
+keymap("v", "<leader>y", '"+y', vim.tbl_extend("force", opts, { desc = "Copy to System Clipboard" }))
+keymap("n", "<leader>p", '"+p', vim.tbl_extend("force", opts, { desc = "Paste from System Clipboard" }))
+
+-- =========================
+-- Delete Unused Default Keymaps
+-- =========================
+del("n", "grt")
+del("n", "grr")
+del("n", "gri")
+del("n", "gra")
+del("n", "grn")
