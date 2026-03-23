ad3230c518036a947232b4bedf81e936d456a5e2
Yuri Henrique B. Maciel <35900428+yurihbm@users.noreply.github.com>
2026-03-23 08:42:38 -0300

    feat(nvim): add command to write without format

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -1,8 +1,18 @@
 local autocmd = vim.api.nvim_create_autocmd
 
+vim.api.nvim_create_user_command("WriteNoFormat", function()
+	vim.b.disable_autoformat_once = true
+	vim.cmd.write()
+end, { desc = "Write current buffer without formatting" })
+
 autocmd("BufWritePre", {
 	pattern = "*",
 	callback = function(args)
+		if vim.b[args.buf].disable_autoformat_once then
+			vim.b[args.buf].disable_autoformat_once = false
+			return
+		end
+
 		require("conform").format({ bufnr = args.buf })
 	end,
 })
