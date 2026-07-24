dbf815e379b59bf263dd1c2206b9b836a77c5722
Yuri <35900428+yurihbm@users.noreply.github.com>
2026-07-24 18:04:49 -0300

    chore: remove linux related config

--- postinstall/scripts/06-gnome.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo -e "\nImporting GNOME settings..."
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-dconf load / <"$SCRIPT_DIR/../../gnome/config.ini"
-
-echo -e "\nGNOME settings import complete.\n"
